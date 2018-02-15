09a333e8d5cc9372adfb6e72cb99aab22eb72d3c
priyaparate <priyanshuparate@yahoo.in>
2018-02-15 08:52:03 +0530

    card service

--- BookLibrary/src/app/card.service.ts
+++ BookLibrary/src/app/card.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class CardService {
 
-  constructor() { }
+  constructor(private fd:AngularFireDatabase) { }
 
+  ListCardsByStatus(status:string){
+   
+  }
 }
